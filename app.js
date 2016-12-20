2e16174d220183b9296a3cf0603b65b5f61258ef
mauri <andres239@gmail.com>
2016-12-20 12:10:05 +0100

    last changes

--- app.js
+++ app.js
@@ -1,4 +1,30 @@
-var initialItems = [
+(function(){
+    var initialItems = 
+
+    'use strict';
+    angular.module("ShoppingCheckOffApp", [])
+    .controller("ShoppingCheckOffController1", ShoppingCheckOffController1)
+    .controller("ShoppingCheckOffController2", ShoppingCheckOffController2)
+    .service("ShoppingCheckOffService", ShoppingCheckOffService)
+
+    ShoppingCheckOffController1.$inject = ['ShoppingCheckOffService']
+    function ShoppingCheckOffController1(ShoppingCheckOffService){
+        var vm = this;
+        vm.items = ShoppingCheckOffService.getToBuyItems();
+        vm.removeItem = function(indexItem){
+            ShoppingCheckOffService.removeItem(indexItem);
+        }
+    };
+
+    ShoppingCheckOffController2.$inject = ['ShoppingCheckOffService']
+    function ShoppingCheckOffController2(ShoppingCheckOffService){
+        var vm = this;
+        vm.items = ShoppingCheckOffService.getBoughtItems();
+    };
+
+    function ShoppingCheckOffService(){
+        var service = this;
+        service.toBuyItems = [
             {
                 name: "Orange Juice",
                 quantity: 1
@@ -21,17 +47,23 @@ var initialItems = [
             },
         ]
 
-(function(){
-    'use strict';
-    angular.module("ShoppingCheckOffApp")
-    .controller("ShoppingCheckOffController1", ShoppingCheckOffController1)
-    .controller("ShoppingCheckOffController2", ShoppingCheckOffController2);
+        service.boughtItems = [];
 
-    function ShoppingCheckOffController1(){
-        var items = initialItems;
-    };
+        service.getToBuyItems = function(){
+            return service.toBuyItems;
+        };
 
-    function ShoppingCheckOffController2(){
+        service.getBoughtItems = function(){
+            return service.boughtItems;
+        };
 
+        service.removeItem = function(indexItem){
+            var item = {
+                name: service.toBuyItems[indexItem].name,
+                quantity: service.toBuyItems[indexItem].quantity
+            }
+            service.boughtItems.push(item);
+            service.toBuyItems.splice(indexItem, 1);
+        };
     };
 })();
